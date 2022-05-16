5634a533e20339dcc2461861de6cbd80495312f0
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-05-16 19:45:52 +0100

    add integers task

---
+++ python-adding-integers/addition.py
@@ -0,0 +1,20 @@
+"""
+This file is part of the tasks for the Adding 2 integers task. It is part of the tasks/projects carried out in 
+the I4GxZuri Scholarship in the Fullstack Track.
+
+
+Author: Mudassir Adili Ahmad (adilimudassir)
+Date: 2022-05-16
+"""
+
+#add two integers
+def add_two_integers(a, b):
+    """
+    This function is the main function of the Adding 2 integers task. It takes two arguments:
+    a - the first number
+    b - the second number
+    and returns the result of the addition.
+    """
+    return a + b
+
+print(add_two_integers(1, 2))
