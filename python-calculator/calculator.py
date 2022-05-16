e39d19fd02a96d262be17dff4f144ba236a6d673
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-05-16 19:47:18 +0100

    add modulo operator

--- python-calculator/calculator.py
+++ python-calculator/calculator.py
@@ -24,6 +24,8 @@ def calculator(a, b, c):
         return a * b
     elif c == '/':
         return a / b
+    elif c == '%':
+        return a % b
     else:
         return 'Invalid operator'
     
