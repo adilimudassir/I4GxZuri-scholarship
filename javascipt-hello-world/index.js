c8df9018ed25ce85e1ee6beb3f2a533118422deb
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-06-21 06:57:37 +0100

    add js hello-world task

---
+++ javascipt-hello-world/index.js
@@ -0,0 +1,20 @@
+
+/*
+This file is part of the tasks for the Introduction to Javascript task.It is part of the tasks / projects carried out in the I4GxZuri Scholarship in the Fullstack Track.
+
+
+Author: Mudassir Adili Ahmad(adilimudassir)
+Date: 2022-96 -21
+*/
+
+
+(() => {
+    alert(`
+    Hello There 👋,
+    
+    My name is Mudassir Adili Ahmad.
+    i'm 5 feet tall.
+    I'm a Nigerian.
+    `)
+})();
+
